68a709c514e4c74d8e2cfe229332f64619d78258
AsadKhan261 <asadkhanasii009@gmail.com>
2022-09-26 12:04:53 +0500

    create logic done

--- lib/auth/create_account.dart
+++ lib/auth/create_account.dart
@@ -1,7 +1,9 @@
+import 'package:auth_login/auth/home_screeen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:loading_overlay/loading_overlay.dart';
 
 class CreateAccount extends StatefulWidget {
   const CreateAccount({Key? key}) : super(key: key);
@@ -14,75 +16,78 @@ TextEditingController nameController = TextEditingController();
 TextEditingController emailController = TextEditingController();
 TextEditingController pwdController = TextEditingController();
 TextEditingController confirmPwdController = TextEditingController();
-
+bool isLoading = false;
 class _CreateAccountState extends State<CreateAccount> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Create Account'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(18.0),
-        child: Column(
-          children: [
-            TextFormField(
-              controller: nameController,
-              decoration: const InputDecoration(
-                hintText: 'Name',
+    return LoadingOverlay(
+      isLoading: isLoading,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Create Account'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Column(
+            children: [
+              TextFormField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  hintText: 'Name',
+                ),
               ),
-            ),
-            TextFormField(
-              controller: emailController,
-              decoration: const InputDecoration(
-                hintText: 'email',
+              TextFormField(
+                controller: emailController,
+                decoration: const InputDecoration(
+                  hintText: 'email',
+                ),
               ),
-            ),
-            TextFormField(
-              controller: pwdController,
-              decoration: const InputDecoration(
-                hintText: 'Password',
+              TextFormField(
+                controller: pwdController,
+                decoration: const InputDecoration(
+                  hintText: 'Password',
+                ),
               ),
-            ),
-            TextFormField(
-              controller: confirmPwdController,
-              decoration: const InputDecoration(
-                hintText: 'Confirm Password',
+              TextFormField(
+                controller: confirmPwdController,
+                decoration: const InputDecoration(
+                  hintText: 'Confirm Password',
+                ),
               ),
-            ),
-            SizedBox(height: 40,),
-            GestureDetector(
-              onTap: () {
-                if(nameController.text.isEmpty){
-                  Fluttertoast.showToast(msg: 'Name Cannot be empty');
-                } else if(emailController.text.isEmpty){
-                  Fluttertoast.showToast(msg: 'Email Cannot be empty');
-                }
-                else if(pwdController.text.isEmpty){
-                  Fluttertoast.showToast(msg: 'password Cannot be empty');
-                }
-                else if(pwdController.text != confirmPwdController.text){
-                  Fluttertoast.showToast(msg: 'password does not match');
-                }else{
-                _signupUser();
-                }
+              SizedBox(height: 40,),
+              GestureDetector(
+                onTap: () {
+                  if(nameController.text.isEmpty){
+                    Fluttertoast.showToast(msg: 'Name Cannot be empty');
+                  } else if(emailController.text.isEmpty){
+                    Fluttertoast.showToast(msg: 'Email Cannot be empty');
+                  }
+                  else if(pwdController.text.isEmpty){
+                    Fluttertoast.showToast(msg: 'password Cannot be empty');
+                  }
+                  else if(pwdController.text != confirmPwdController.text){
+                    Fluttertoast.showToast(msg: 'password does not match');
+                  }else{
+                  _signupUser();
+                  }
 
-              },
-              child: Container(
-                height: 50,
-                width: 200,
-                color: Colors.red,
-                child: const Center(child: Text('Create account')),
-              ),
-            )
-          ],
+                },
+                child: Container(
+                  height: 50,
+                  width: 200,
+                  color: Colors.red,
+                  child: const Center(child: Text('Create account')),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
   }
   _signupUser() async {
     try {
-
+   loadingTrue();
       await FirebaseAuth.instance
           .createUserWithEmailAndPassword(
           email: emailController.text, password: pwdController.text)
@@ -98,11 +103,22 @@ class _CreateAccountState extends State<CreateAccount> {
           "joiningDate": DateTime.now().millisecondsSinceEpoch
         });
       }).then((value) async {
-
+        loadingFalse();
+     Navigator.push(context, MaterialPageRoute(builder: (context)=>HomeScreen()));
       });
     } on FirebaseException catch (e) {
-
+     loadingFalse();
       Fluttertoast.showToast(msg: e.message!);
     }
   }
+   void loadingFalse(){
+    isLoading = false;
+    setState(() {
+
+    });
+   }
+void loadingTrue(){
+    isLoading = true;
+    setState(() {});
+}
 }
